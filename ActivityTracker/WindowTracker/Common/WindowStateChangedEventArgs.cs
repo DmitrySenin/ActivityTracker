8010bfbcd404fc0eec2f9385ef2372d16bf69305
dmitry.senin <white.lord.mane@gmail.com>
2017-01-17 01:19:50 +0300

    Implemented basic logic of tracking and checked in console application

---
+++ ActivityTracker/WindowTracker/Common/WindowStateChangedEventArgs.cs
@@ -0,0 +1,23 @@
+﻿namespace WindowTracker.Common
+{
+    public class WindowStateChangedEventArgs
+    {
+        #region Constructos
+
+        public WindowStateChangedEventArgs(string title, string processName)
+        {
+            this.Title = title;
+            this.ProcessName = processName;
+        }
+
+        #endregion
+
+        #region Public Fields And Properties
+
+        public string Title { get; private set; }
+
+        public string ProcessName { get; private set; }
+
+        #endregion
+    }
+}
---
+++ ActivityTracker/WindowTracker/Trackers/IWindowStateTracker.cs
@@ -0,0 +1,23 @@
+﻿namespace WindowTracker.Trackers
+{
+    using System;
+
+    using Common;
+
+    public interface IWindowStateTracker
+    {
+        #region Delegates And Events
+
+        event EventHandler<WindowStateChangedEventArgs> WindowStateChanged;
+
+        #endregion
+
+        #region Public Methods
+
+        void StartTracking();
+
+        void StopTracking();
+
+        #endregion
+    }
+}

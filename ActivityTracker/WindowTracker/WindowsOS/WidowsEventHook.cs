03c7c1e5e5f210b994b5652b944cc3b8a7f07f31
dmitry.senin <white.lord.mane@gmail.com>
2017-01-17 01:19:01 +0300

    Wraped complex logic of WinApi hooks inside one class

---
+++ ActivityTracker/WindowTracker/WindowsOS/WidowsEventHook.cs
@@ -0,0 +1,61 @@
+﻿namespace WindowTracker.WindowsOS
+{
+    using System;
+
+    using Common;
+
+    internal class WidowsEventHook : IOperationSystemHook<Window>, IDisposable
+    {
+        #region Delegates And Events
+
+        public event EventHandler<Window> NewEvent;
+
+        #endregion
+
+        #region  Private Fields And Properties
+
+        private IntPtr HookIdentifier { get; set; }
+
+        private WinApiEvents TrackedEvent { get; set; }
+
+        private uint TrackedProcessId { get; set; }
+
+        #endregion
+
+        #region Constructos
+
+        public WidowsEventHook(WinApiEvents trackedEvent, uint processId = 0)
+        {
+            this.TrackedEvent = trackedEvent;
+            this.TrackedProcessId = processId;
+
+            uint eventCode = (uint) this.TrackedEvent;
+            this.HookIdentifier = WinApiWrapper.SetWinEventHook(eventCode, eventCode, IntPtr.Zero, this.HookBody,
+                this.TrackedProcessId, 0, WidowsEventHook.WinEventOutOfContext);
+        }
+
+        #endregion
+
+        #region Methods
+
+        private void HookBody(
+            IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread,
+            uint dwmsEventTime)
+        {
+            this.NewEvent?.Invoke(this, new Window(hwnd));
+        }
+
+        #endregion
+
+        #region Public Methods
+
+        public void Dispose()
+        {
+            WinApiWrapper.UnhookWinEvent(this.HookIdentifier);
+        }
+
+        #endregion
+
+        private const uint WinEventOutOfContext = 0;
+    }
+}

f118ce2f2f70705b590163dc1a89aedca9d9bda6
dmitry.senin <white.lord.mane@gmail.com>
2017-01-17 03:03:04 +0300

    SetUp DB and performed migrations

---
+++ ActivityTracker/TrackingSystem/Database/ActivityContext.cs
@@ -0,0 +1,20 @@
+﻿namespace TrackingSystem.Database
+{
+    using System.Data.Entity;
+
+    internal class ActivityContext: DbContext
+    {
+        #region Constructos
+
+        public ActivityContext() : base("ActivityTracker")
+        {}
+
+        #endregion
+
+        #region Public Fields And Properties
+
+        public DbSet<Activity> Activities { get; set; }
+
+        #endregion
+    }
+}
---
+++ ActivityTracker/TrackingSystem/Database/ActivityStorage.cs
@@ -0,0 +1,35 @@
+﻿namespace TrackingSystem.Database
+{
+    using System;
+    using System.Collections.Generic;
+    using System.Linq;
+
+    public class ActivityStorage
+    {
+        #region Public Methods
+
+        public void Add(Activity activity)
+        {
+            TimeSpan offset = activity.EndDate - activity.StartDate;
+
+            if (offset.TotalSeconds > 30)
+            {
+                using (var context = new ActivityContext())
+                {
+                    context.Activities.Add(activity);
+                    context.SaveChanges();
+                }
+            }
+        }
+
+        public IList<Activity> GetActivities()
+        {
+            using (var context = new ActivityContext())
+            {
+                return context.Activities.ToList();
+            }
+        }
+
+        #endregion
+    }
+}

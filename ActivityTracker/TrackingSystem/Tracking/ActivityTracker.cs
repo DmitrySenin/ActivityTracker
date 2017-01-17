979e9d54ac7b3dcba510607b19775590d9d43aac
dmitry.senin <white.lord.mane@gmail.com>
2017-01-17 03:03:34 +0300

    Added activity tracker that inscapsulates all logic of activity creation

---
+++ ActivityTracker/TrackingSystem/Tracking/ActivityTracker.cs
@@ -0,0 +1,59 @@
+﻿namespace TrackingSystem.Tracking
+{
+    using System;
+
+    using Database;
+
+    using WindowTracker.Trackers;
+    using WindowTracker.WindowsOS;
+
+    public class ActivityTracker
+    {
+        private IWindowStateTracker WindowTracker { get; set; }
+
+        private Activity CurrentActivity { get; set; }
+
+        public ActivityTracker()
+        {
+            this.WindowTracker = new ActiveWindowStateTracker();
+            this.WindowTracker.WindowStateChanged += this.WindowTracker_WindowStateChanged;
+        }
+
+        private void WindowTracker_WindowStateChanged(object sender, WindowTracker.Common.WindowStateChangedEventArgs e)
+        {
+            if (this.CurrentActivity == null)
+            {
+                this.CurrentActivity = new Activity()
+                {
+                    WindowTitle = e.Title,
+                    ProcessName = e.ProcessName,
+                    StartDate = DateTime.UtcNow
+                };
+
+                return;
+            }
+
+            this.CurrentActivity.EndDate = DateTime.UtcNow;
+
+            var storage = new ActivityStorage();
+            storage.Add(this.CurrentActivity);
+
+            this.CurrentActivity = new Activity()
+            {
+                WindowTitle = e.Title,
+                ProcessName = e.ProcessName,
+                StartDate = DateTime.UtcNow
+            };
+        }
+
+        public void StartTracking()
+        {
+            this.WindowTracker.StartTracking();
+        }
+
+        public void StopTracking()
+        {
+            this.WindowTracker.StopTracking();
+        }
+    }
+}
